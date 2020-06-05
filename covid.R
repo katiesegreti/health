0999844c463797b182d65a10ebc7f430216c4ffc
Katie <katie.segreti@gmail.com>
2020-06-05 08:32:33 -0400

    Add covid.R

---
+++ covid.R
@@ -0,0 +1,26 @@
+library(readr)
+library(dplyr)
+library(purrr)
+library(ggplot2)
+library(lubridate)
+
+testing <- read_csv("https://raw.githubusercontent.com/katiesegreti/health/master/New_York_State_Statewide_COVID-19_Testing.csv")
+nyc_covid <- read_csv("https://raw.githubusercontent.com/katiesegreti/health/master/COVID-19_Daily_Counts_of_Cases__Hospitalizations__and_Deaths.csv")
+
+
+str(testing)
+testing$County <- as.factor(testing$County)  
+
+str(nyc_covid)
+
+x <- nyc_covid$DATE_OF_INTEREST[2]
+
+unlist(strsplit(x, split = " "))[1]
+
+y <- strsplit(x, split = " ")[[1]][1]
+mdy(y)
+
+
+nyc_covid <- nyc_covid %>%
+  mutate(date_interest = unlist(strsplit(DATE_OF_INTEREST, split = " ")[1]))
+
