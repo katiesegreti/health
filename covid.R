4d587341d6e3ae0f051b52dd70a62f2f2c5b5676
Katie <katie.segreti@gmail.com>
2020-06-09 08:36:02 -0400

    Update covid.R

--- covid.R
+++ covid.R
@@ -4,6 +4,7 @@ library(purrr)
 library(ggplot2)
 library(lubridate)
 library(stringr)
+library(janitor)
 
 testing <- read_csv("https://raw.githubusercontent.com/katiesegreti/health/master/New_York_State_Statewide_COVID-19_Testing.csv")
 nyc_covid <- read_csv("https://raw.githubusercontent.com/katiesegreti/health/master/COVID-19_Daily_Counts_of_Cases__Hospitalizations__and_Deaths.csv")
@@ -16,5 +17,11 @@ str(nyc_covid)
 
 
 nyc_covid <- nyc_covid %>%
-  mutate(date_interest = str_split(DATE_OF_INTEREST, pattern = " ", simplify = TRUE)[,1])
+  mutate(date_interest = mdy(str_split(DATE_OF_INTEREST, pattern = " ", simplify = TRUE)[,1]))
 
+nassau <- testing %>%
+  filter(County == "Nassau")
+
+testing <- testing %>% make_clean_names()
+
+names(testing) <- make_clean_names(names(testing))
