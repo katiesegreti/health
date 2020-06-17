344206806a8c4afa3ae3c86a31e193d221c39941
Katie <katie.segreti@gmail.com>
2020-06-17 08:33:27 -0400

    Update covid.R

--- covid.R
+++ covid.R
@@ -7,8 +7,12 @@ library(stringr)
 library(janitor)
 library(ggthemes)
 library(zoo)
+library(tidyr)
+library(plotly)
 
 testing <- read_csv("https://raw.githubusercontent.com/katiesegreti/health/master/New_York_State_Statewide_COVID-19_Testing.csv")
+testing1 <- read_csv("https://health.data.ny.gov/resource/xdss-u53e.csv")
+
 nyc_covid <- read_csv("https://raw.githubusercontent.com/katiesegreti/health/master/COVID-19_Daily_Counts_of_Cases__Hospitalizations__and_Deaths.csv")
 
 nyc_covid <- nyc_covid %>%
@@ -23,7 +27,7 @@ testing <- testing %>%
   mutate(test_date = mdy(test_date),
          pct_pos_today = if_else((new_positives > 0 & total_number_of_tests_performed > 0),
                                  new_positives / total_number_of_tests_performed, 0)
-         )
+  )
 
 
 #add rolling average column
@@ -46,37 +50,29 @@ counties_theme <- theme_wsj() +
     axis.ticks.x.bottom = element_blank(),
     panel.grid.major.y = element_line( colour = "darkgray"),
     plot.subtitle = element_text(size = 18),
-    plot.title = element_text(size = 22)
+    plot.title = element_text(size = 22),
+    plot.caption = element_text(size = 12)
   )
 
-nassau <- testing %>%
-  filter(county == "Nassau")
-
-nassau %>% 
-  ggplot(aes(x = test_date, y = new_positives)) +
-  geom_col(fill = "dodgerblue") +
-  counties_theme +
-  labs(
-    x = "",
-    y = "",
-    title = "Positive COVID-19 Tests",
-    subtitle = "Nassau County, NY"
+counties_theme_stacked <- theme_wsj() +
+  theme(
+    panel.background = element_rect(fill = bg_color),
+    plot.background = element_rect(fill = bg_color),
+    strip.background = element_rect(fill = bg_color),
+    #axis.text.x = element_blank(),
+    legend.position = "top",
+    #legend.key.size = unit(0.2, "npc"),
+    legend.title = element_blank(),
+    #legend.direction = "vertical",
+    legend.background = element_rect(fill = bg_color),
+    axis.line.x.bottom = element_blank(),
+    axis.ticks.x.bottom = element_blank(),
+    panel.grid.major.y = element_line( colour = "darkgray"),
+    plot.subtitle = element_text(size = 18),
+    plot.title = element_text(size = 22),
+    plot.caption = element_text(size = 12)
   )
 
-nassau %>% 
-  ggplot(aes(x = test_date, y = total_number_of_tests_performed)) +
-  geom_col(fill = "magenta") +
-  counties_theme +
-  labs(
-    x = "",
-    y = "",
-    title = "Positive COVID-19 Tests By Day",
-    subtitle = "Nassau County, NY"
-  )
-
-nassau %>% 
-  ggplot(aes(x = test_date, y = pct_pos_today)) +
-  geom_col()
 
 testing %>% filter(county == "Nassau") %>%
   ggplot(aes(x = test_date, y = new_positives)) +
@@ -87,7 +83,8 @@ testing %>% filter(county == "Nassau") %>%
     x = "",
     y = "",
     title = "Positive COVID-19 Tests By Day",
-    subtitle = "Nassau County, NY"
+    subtitle = "Nassau County, NY",
+    caption = "data-chips.com"
   )
 
 
@@ -116,7 +113,7 @@ testing %>% filter(county == "Suffolk") %>%
     subtitle = "Suffolk County, NY"
   )
 
-testing %>% filter(county == "Kings") %>%
+bk <- testing %>% filter(county == "Kings") %>%
   ggplot(aes(x = test_date, y = new_positives)) +
   geom_col(fill = "magenta", width = 0.8) +
   geom_line(aes(y = rollmean(new_positives, 7, fill = NA)), color = "midnightblue", size = 1.3) +
@@ -188,7 +185,30 @@ testing %>% filter(county == "Rockland") %>%
     subtitle = "Rockland County, NY"
   )
 
+
+#filter testing data by county function
+county_data <- function(county_selected) {
+  filter(testing, county == county_selected)
+}
+
+county_data("Queens")
+
+
+testing %>% filter(county == "Nassau") %>%
+  ggplot(aes(x = test_date, y = total_number_of_tests_performed)) +
+  geom_col(fill = "magenta", width = 0.8) +
+  geom_line(aes(y = rollmean(total_number_of_tests_performed, 7, fill = NA)), color = "midnightblue", size = 1.3) +
+  counties_theme +
+  labs(
+    x = "",
+    y = "",
+    title = "Number of Tests Performed By Day",
+    subtitle = "Nassau County, NY"
+  )
+
+
 #stacked bar charts
+
 
 testing_wide <- testing %>%
   select(test_date, county, new_positives, total_number_of_tests_performed) %>%
@@ -204,7 +224,77 @@ testing_long %>%
   ggplot(aes(x = test_date, y = value, fill = result)) +
   geom_col() 
 
-testing_long %>%
+t <- testing_long %>%
   filter(county == "Nassau") %>%
   ggplot(aes(x = test_date, y = value, fill = result)) +
-  geom_col() 
+  geom_col(width = 0.8) +
+  scale_fill_manual(values = c("darkgrey", "magenta"))+
+  counties_theme_stacked +
+  labs(
+    x = "",
+    y = "",
+    title = "Tests Performed By Day",
+    subtitle = "Nassau County, NY"
+  )
+
+max(testing$test_date)
+
+
+library(plotly)
+
+fig <- plot_ly(
+  x = c("giraffes", "orangutans", "monkeys"),
+  y = c(20, 14, 23),
+  name = "SF Zoo",
+  type = "bar"
+)
+
+fig
+
+
+ggplotly(t)
+ggplotly(bk)
+
+
+county_data <- function(county_selected) {
+  filter(testing, county == county_selected) %>%
+    mutate(seven_day_avg = rollmean(new_positives, 7, fill = NA))
+}
+
+
+
+county_data("Queens")
+
+
+
+connecticut <- read_csv("https://data.ct.gov/resource/bfnu-rgqt.csv")
+str(connecticut)
+
+
+
+
+
+library(rjson)
+ny_json <- do.call(rbind.data.frame, fromJSON(file = "https://health.data.ny.gov/resource/xdss-u53e.json") )
+
+str(ny_json)
+mdy(as_date(ny_json$test_date))
+
+ny_json1 <- ny_json %>%
+  mutate(test_date = mdy(test_date),
+         pct_pos_today = if_else((new_positives > 0 & total_number_of_tests_performed > 0),
+                                 new_positives / total_number_of_tests_performed, 0)
+  )
+
+ny_json1 <- ny_json %>%
+  mutate(test_date = ymd(str_split(test_date, pattern = "T", simplify = TRUE)[,1]),
+         county = as.factor(county),
+         new_positives = as.numeric(new_positives),
+         cumulative_number_of_positives = as.numeric(cumulative_number_of_positives),
+         total_number_of_tests = as.numeric(total_number_of_tests),
+         cumulative_number_of_tests = as.numeric(cumulative_number_of_tests),
+         pct_pos_today = if_else((new_positives > 0 & total_number_of_tests > 0),
+                                 new_positives / total_number_of_tests, 0))
+
+
+str(ny_json1)
